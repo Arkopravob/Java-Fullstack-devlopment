075ad75759e16d3762c150dce64e4877a8f519ed
Arkopravob <arka.bhuin@gmail.com>
2025-07-22 20:24:46 +0530

    Initial commit for Week1 Core Java

---
+++ src/Day1_Variables/VariablesDemo.java
@@ -0,0 +1,9 @@
+package Day1_Variables;
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+        System.out.println("start variables demo");
+        int i =  10;
+        System.out.println(i);
+    }
+}
