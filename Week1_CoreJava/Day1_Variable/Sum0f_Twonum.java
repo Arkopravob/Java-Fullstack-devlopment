f72dab575c41ae9242f5bfcd1ff8d77063fed893
Arkopravob <arka.bhuin@gmail.com>
2025-08-03 08:44:50 +0530

    update code

---
+++ Week1_CoreJava/Day1_Variable/Sum0f_Twonum.java
@@ -0,0 +1,10 @@
+package Day1_Variable;
+
+public class Sum0f_Twonum {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 13;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
---
+++ Week1_CoreJava/Day2_Operator_loop/Operator_Demo.java
@@ -0,0 +1,7 @@
+package Day2_Operator_loop;
+
+public class Operator_Demo {
+    public static void main(String[] args) {
+
+    }
+}
