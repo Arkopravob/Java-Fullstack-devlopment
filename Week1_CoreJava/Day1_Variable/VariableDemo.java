0765511c98e270e877e743aae79a18d334268106
Arkopravob <arka.bhuin@gmail.com>
2025-08-03 08:45:27 +0530

    update code

--- Week1_CoreJava/Day1_Variable/VariableDemo.java
+++ Week1_CoreJava/Day1_Variable/VariableDemo.java
@@ -1,4 +1,16 @@
 package Day1_Variable;
 
 public class VariableDemo {
+    public static void main(String[] args) {
+        System.out.println("start Day 1");
+        //boolean data type
+        boolean b1 = true;
+        boolean b2 = false;
+
+        System.out.println("is java make fun ?" + b1);
+        System.out.println(" are you a girl? " + b2);
+
+        //byte;
+        
+    }
 }
