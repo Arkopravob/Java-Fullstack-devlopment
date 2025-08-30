29cec03d65f8264d7ea19d87c1ffcf66591782f2
Arkopravob <arka.bhuin@gmail.com>
2025-08-30 17:58:18 +0530

    shift operator

---
+++ Week1_CoreJava/Day3_operator/ShiftOps.java
@@ -0,0 +1,12 @@
+package Day3_operator;
+
+public class ShiftOps {
+    public static void main(String[] args) {
+        int a = 10;
+        //using left shift
+        System.out.println("a+1: "+ (a<<1));
+
+        //right shift
+        System.out.println("a+1: " + (a>>1) );
+    }
+}
