90a6fdecb7cc6f9da8c920c83663d99ac51ab054
Arkopravo Bhuin <99447101+Arkopravob@users.noreply.github.com>
2026-01-18 12:39:36 +0530

    BinarySearch code

--- Week2_CoreJava/algorithm/BinarySearch.java
+++ Week2_CoreJava/algorithm/BinarySearch.java
@@ -1,4 +1,38 @@
 package algorithm;
 
 public class BinarySearch {
+    public static int search(int a[], int key){
+        int l = 0 ;
+        int r = a.length-1;
+
+        while(l <= r){
+            int mid = (r+l)/2;
+            if(a[mid] ==key){
+                return mid;
+            } else if (a[mid] < key) {
+                l = mid + 1;
+
+            } else  {
+                r = mid -1;
+
+            }
+
+        }
+        return -1;
+
+
+    }
+    public static void main(String[] args) {
+        int a[] = {25,57,19,21,40};
+        int n = a.length;
+//        int key = 40;
+
+
+        int result = search(a,40);
+        if(result == - 1)
+        System.out.println("element not present in an array");
+        else{
+            System.out.println("element present at index " + result );
+        }
+    }
 }
