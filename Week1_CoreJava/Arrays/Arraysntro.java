aa6c33391fbc910e2809f47fa3c9437fd36a0517
Arkopravo Bhuin <99447101+Arkopravob@users.noreply.github.com>
2026-01-19 18:25:56 +0530

    array

---
+++ Week1_CoreJava/Arrays/Arraysntro.java
@@ -0,0 +1,12 @@
+package Arrays;
+import java.util.*;
+public class Arraysntro {
+    public static void main(String[] args) {
+        int [] intArray;
+        intArray = new int [10];
+        for(int i = 0 ;  i < intArray.length ;i++){
+            System.out.println(intArray[i]);
+        }
+
+    }
+}
