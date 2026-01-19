b7d94e6024ddb98691a748083d8aa881ac88edff
Arkopravo Bhuin <99447101+Arkopravob@users.noreply.github.com>
2026-01-19 18:26:42 +0530

    new function code update

--- Week2_CoreJava/functionLearning/FunctionDemo2.java
+++ Week2_CoreJava/functionLearning/FunctionDemo2.java
@@ -1,4 +1,19 @@
 package functionLearning;
 
 public class FunctionDemo2 {
+    public static  int findMax(int x, int y){
+        if(x>y){
+            return x;
+
+        }
+        else{
+            return y;
+        }
+    }
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 36;
+        int res = findMax(a,b);
+        System.out.println(res);
+    }
 }
